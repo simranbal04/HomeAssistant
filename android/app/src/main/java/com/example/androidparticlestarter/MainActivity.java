a801b559909285905e216af76d9f748c00f130a8
Simranbal04 <simranbal01@gmail.com>
2019-10-23 16:41:18 -0400

    initial commit

---
+++ android/app/src/main/java/com/example/androidparticlestarter/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.androidparticlestarter;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
+import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.cloud.ParticleEvent;
+import io.particle.android.sdk.cloud.ParticleEventHandler;
+import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
+import io.particle.android.sdk.utils.Async;
+
+public class MainActivity extends AppCompatActivity {
+    // MARK: Debug info
+    private final String TAG="";
+
+    // MARK: Particle Account Info
+    private final String PARTICLE_USERNAME = "";
+    private final String PARTICLE_PASSWORD = "";
+
+    // MARK: Particle device-specific info
+    private final String DEVICE_ID = "";
+
+    // MARK: Particle Publish / Subscribe variables
+    private long subscriptionId;
+
+    // MARK: Particle device
+    private ParticleDevice mDevice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 1. Initialize your connection to the Particle API
+        ParticleCloudSDK.init(this.getApplicationContext());
+
+        // 2. Setup your device variable
+        getDeviceFromCloud();
+
+    }
+
+
+    /**
+     * Custom function to connect to the Particle Cloud and get the device
+     */
+    public void getDeviceFromCloud() {
+        // This function runs in the background
+        // It tries to connect to the Particle Cloud and get your device
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                particleCloud.logIn(PARTICLE_USERNAME, PARTICLE_PASSWORD);
+                mDevice = particleCloud.getDevice(DEVICE_ID);
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+                Log.d(TAG, "Successfully got device from Cloud");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
+    }
+
+}
